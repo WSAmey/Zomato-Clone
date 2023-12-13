a1a7b6053766249e3022ae916f2f6daf6df37d46
Amey S Wairagade <138147791+WSAmey@users.noreply.github.com>
2023-12-13 19:21:46 +0530

    Delete frontend-client/src/components/Day 77 DIY directory

--- frontend-client/src/components/Day 77 DIY/InlineComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const InlineComponent = () => {
-  return (
-    <div style={{padding:"50px",color:"white",background:"skyblue",fontWeight:"bold"}}>Inline CSS</div>
-  )
-}
-
-export default InlineComponent
