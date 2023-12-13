952e71bf04eea656a991fd0eb20ecdbbcd5d3355
Amey S Wairagade <138147791+WSAmey@users.noreply.github.com>
2023-12-13 19:19:53 +0530

    Delete frontend-client/src/components/Day 70 DIY directory

--- frontend-client/src/components/Day 70 DIY/Classcomp.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Classcomp() {
-  return (
-    <div>It   is   my   first   class component </div>
-  )
-}
-
-export default Classcomp
--- frontend-client/src/components/Day 70 DIY/Funccomponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Funccomponent() {
-  return (
-    <div>it   is   my   first  functional  component</div>
-  )
-}
-
-export default Funccomponent
